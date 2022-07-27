1f4f81d0f1340342d2145caf873a3c0f75610b0f
Christopher DiSalle <45129936+cdisall@users.noreply.github.com>
2022-07-27 11:40:31 -0400

    Add solution

---
+++ LongestCommonPrefix.py
@@ -0,0 +1,7 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        s = strs[0]
+        for i in strs:
+            while not i.startswith(s):
+                s = s[:-1]
+        return s
