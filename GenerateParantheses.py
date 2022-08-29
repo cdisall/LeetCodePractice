589a2b8bbd0eaefb0911963ba8edcc729aef248c
Christopher DiSalle <45129936+cdisall@users.noreply.github.com>
2022-08-28 23:55:38 -0400

    Add solution

---
+++ GenerateParantheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        l = []
+        self.backtrack('', 0, 0, l, n)
+        return l
+
+    def backtrack(self, s, left, right, l, n):
+        if len(s) == 2 * n:
+            l.append(s)
+            return
+        if left < n:
+            self.backtrack(s + '(', left + 1, right, l, n)
+        if right < left:
+            self.backtrack(s + ')', left, right + 1, l, n)
+
