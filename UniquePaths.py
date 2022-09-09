4bbfdf7aff9077bc77d482c64513a28bc3c6b0fb
CD <christopher.disalle@mail.mcgill.ca>
2022-09-09 12:39:52 -0400

    Update solution

--- UniquePaths.py
+++ UniquePaths.py
@@ -1,13 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return self.dp(m, n, 0, 0)
-    
-    def dp(self, m, n, i, j):
-        if i == m-1 and j == n-1:
-            return 1
-        if i < m-1 and j < n-1:
-            return self.dp(m, n, i+1, j) + self.dp(m, n, i, j+1)
-        if i < m and j >= n-1:
-            return self.dp(m, n, i+1, j)
-        if i >= m-1 and j < n:
-            return self.dp(m, n, i, j+1)
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            dp[i][0] = 1
+        for i in range(n):
+            dp[0][i] = 1
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
