7b33664c8a0330ecd72a7060a3c329c1290f220d
Christopher DiSalle <45129936+cdisall@users.noreply.github.com>
2022-10-02 15:58:20 +0200

    Add solution

---
+++ MinionGame.py
@@ -0,0 +1,15 @@
+def minion_game(string):
+    stuart = 0
+    kevin = 0
+    vowels = ["A", "E", "I", "O", "U"]
+    for i in range(len(string)):
+        if string[i] in vowels:
+            kevin += len(string)-i
+        else:
+            stuart += len(string)-i
+    if (kevin > stuart):
+        print("Kevin ", kevin)
+    elif (kevin < stuart):
+        print("Stuart", stuart)
+    else:
+        print("Draw")
