8f70511456cea9443fb809ed3e0cafbce52d66e0
Christopher DiSalle <45129936+cdisall@users.noreply.github.com>
2022-10-04 05:44:01 -0400

    Add solution

---
+++ TimeConversion.py
@@ -0,0 +1,7 @@
+def timeConversion(s):
+    if s[-2:] == "AM" and s[0:2] == "12":
+        return("00" + s[2:-2])
+    elif (s[-2:] == "PM" and s[0:2] == "12") or s[-2:] == "AM":
+        return(str(s[:-2]))
+    elif s[-2:] == "PM":
+        return(str(int(s[:2])+12)+s[2:-2])
