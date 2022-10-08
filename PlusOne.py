7f1cbc22f15eae865729d2dbae4c6d00fd8e08ab
CD <christopher.disalle@mail.mcgill.ca>
2022-10-08 15:28:02 -0400

    Add solution

---
+++ PlusOne.py
@@ -0,0 +1,21 @@
+class Solution:
+    def plusOne(self, digits):
+        if digits[-1] < 9:
+            digits[-1] += 1
+            return digits
+        else:
+            for i in range(len(digits)-1, -1, -1):
+                if digits[i] < 9:
+                    digits[i] += 1
+                    return digits
+                else:
+                    digits[i] = 0
+                if i == 0: 
+                    return [1] + digits
+
+def main():
+    s = Solution()
+    print(s.plusOne([9]))
+
+if __name__ == "__main__":
+    main()
