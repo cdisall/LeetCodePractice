8ed25c0de643f01e4527ae46e1564166c45d7fb0
Christopher DiSalle <45129936+cdisall@users.noreply.github.com>
2022-10-11 22:48:35 -0400

    Update solution

--- Subsets.py
+++ Subsets.py
@@ -4,3 +4,8 @@ class Solution:
         for i in nums:
             res += [[i] + s for s in res]
         return res
+def main():
+     s = Solution()
+     print(s.subsets([1,4,5,8])
+if __name__ == "__main__":
+     main()
